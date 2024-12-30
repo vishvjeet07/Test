4bc7937371ec3360f539dab471dc1a294641e8de
vishvjeet07 <vishvjeetwankhade07@gmail.com>
2024-12-30 08:41:07 +0530

    4

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
-app.get('/',(req,res)=>{
-    res.send("hey bro");
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-app.listen(3000);
+// Export the app as a Vercel-compatible handler
+module.exports = app;
