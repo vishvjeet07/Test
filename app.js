e5ec140b8b02d90310b2b0da15c82451180a09f1
vishvjeet07 <vishvjeetwankhade07@gmail.com>
2024-12-30 08:57:31 +0530

    sixth

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mainRoute = require('./routes/app');
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 
-
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use('/',mainRoute);
 
 // Export the app as a Vercel-compatible handler
 module.exports = app;
