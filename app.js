64dcdeb4be253a33e850f0f6014486890d09b0ab
vishvjeet07 <vishvjeetwankhade07@gmail.com>
2024-12-30 08:42:58 +0530

    5

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
+
 
 app.get('/', (req, res) => {
     res.render('index');
